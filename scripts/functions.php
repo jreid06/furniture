6b6d377814d42bc183dabfd173083bf087c51774
jason reid <jasonbrandon9406@gmail.com>
2018-02-27 10:12:42 +0000

    added function to get stripe products

--- scripts/functions.php
+++ scripts/functions.php
@@ -123,6 +123,119 @@
 	    return $timeArray;
 	}
 
+	function get_products($amount){
+		// get products from stripe api
+        \Stripe\Stripe::setApiKey("sk_test_o3lzBtuNJXFJOnmzNUfNjpXW");
+        // \Stripe\Stripe::setApiKey("sk_test_o3lzBtuNJXFJOnmzNUfNjpXW");
+        try {
+            // $products_all = \Stripe\Product::retrieve("prod_CDME1dinY2YyTr");
+            $products_all = \Stripe\Product::all(array(
+                "limit" => $amount
+            ));
+
+            // for ($i=0; $i < count($products_all['data']); $i++) {
+                //     echo $products_all['data'][$i]['id']."<br>";
+                //     echo $products_all['data'][$i]['name']."<br>";
+                //     echo $products_all['data'][$i]['skus']['data'][$i]['id']."<br>";
+                //     echo "<br>";
+                // }
+
+            // // // / / / /// /// // // // // // // //
+
+            $msg = array(
+                'status'=> 'success',
+                'product'=> $products_all['data']
+            );
+
+			return $msg;
+
+        } catch(Stripe_CardError $e) {
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+
+            $error1 = $body['error']['type'];
+
+
+            $msg = array(
+                'status'=> 'error',
+                'product'=> $error1
+            );
+
+            return $msg;
+
+        } catch (Stripe_InvalidRequestError $e) {
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+              // Invalid parameters were supplied to Stripe's API
+              $error2 = $body['error']['type'];
+
+              $msg = array(
+                  'status'=> 'error',
+                  'product'=> $error2
+              );
+
+              return $msg;
+
+        } catch (Stripe_AuthenticationError $e) {
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+          // Authentication with Stripe's API failed
+          $error3 = $body['error']['type'];
+
+          $msg = array(
+              'status'=> 'error',
+              'product'=> $error3
+          );
+
+          return $msg;
+
+        } catch (Stripe_ApiConnectionError $e) {
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+          // Network communication with Stripe failed
+          $error4 = $body['error']['type'];
+
+          $msg = array(
+              'status'=> 'error',
+              'product'=> $error4
+          );
+
+          return $msg;
+
+        } catch (Stripe_Error $e) {
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+          // Display a very generic error to the user, and maybe send
+          // yourself an email
+          $error5 = $body['error']['type'];
+
+          $msg = array(
+              'status'=> 'error',
+              'product'=> $error5
+          );
+
+          return $msg;
+
+
+        } catch (Exception $e) {
+            $body = $e->getJsonBody();
+            // var_dump($body);
+          // Something else happened, completely unrelated to Stripe
+          $error6 = $body['error']['type'];
+          $err_msg =  $body['error']['message'];
+
+          $msg = array(
+              'status'=> 'error',
+              'error_type'=> $error6,
+              'error_msg'=> $err_msg
+          );
+
+          return $msg;
+
+      }
+
+	}
+
 	function send_receipt($charge, $email){
 
 		try {
