da637da76b4d516e76900ed303e6a9f194f2dd07
jason reid <jasonbrandon9406@gmail.com>
2018-03-01 18:08:36 +0000

    removed duplicate meta charset

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,6 @@
 		<meta charset="utf-8">
 		<title>IDYL - Furniture</title>
 
-		<meta charset="utf-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 
 	    <meta name="description" content="">
