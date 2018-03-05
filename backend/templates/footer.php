35a925de1225ae3bc253100933cfac4d7261218e
jason reid <jasonbrandon9406@gmail.com>
2018-03-05 12:30:56 +0000

    added scripts to footer

--- backend/templates/footer.php
+++ backend/templates/footer.php
@@ -7,9 +7,17 @@
 
     <!-- Simple MDE plugin -->
     <script src="/node_modules/simplemde/dist/simplemde.min.js" charset="utf-8"></script>
+
+    <!-- file upload plugins -->
     <script src="/js/plugins/fileupjs/vendor/jquery.ui.widget.js"></script>
+    <script src="/js/plugins/fileupjs/load-image.all.min.js"></script>
+    <script src="/js/plugins/fileupjs/canvas-to-blob.min.js"></script>
     <script src="/js/plugins/fileupjs/jquery.iframe-transport.js"></script>
     <script src="/js/plugins/fileupjs/jquery.fileupload.js"></script>
+    <script src="/js/plugins/fileupjs/jquery.fileupload-process.js"></script>
+    <script src="/js/plugins/fileupjs/jquery.fileupload-image.js"></script>
+    <script src="/js/plugins/notify.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
     <!-- <script src="/backend/vendor/metisMenu/metisMenu.min.js"></script> -->
