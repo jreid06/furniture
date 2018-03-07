03fce25bc1edceb4fa748b1d6cba204049c95df5
jason reid <jasonbrandon9406@gmail.com>
2018-03-07 14:08:34 +0000

    new nav file for edit content pages; nav has vue instance added directly

---
+++ backend/templates/nav-vue.php
@@ -0,0 +1,95 @@
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-static-top admin-nav dash-vue" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+        <!-- <img src="/assets/general/idyl_original_logo.png" alt="idyl logo" width="80" height="45" style="margin-top:2px; padding-left: 15px;" @click="dashhome"> -->
+        <p class="text-center"> <span class="fa fa-home" @click="dashhome"></span></p>
+
+    </div>
+    <!-- /.navbar-header -->
+
+    <ul class="nav navbar-top-links navbar-right">
+
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+                <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                </li>
+                <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                </li>
+                <li class="divider"></li>
+                <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                </li>
+            </ul>
+            <!-- /.dropdown-user -->
+        </li>
+        <!-- /.dropdown -->
+    </ul>
+    <!-- /.navbar-top-links -->
+
+    <div class="navbar-default sidebar admin-nav" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+                <!-- <li class="sidebar-search">
+                    <div class="input-group custom-search-form">
+                        <input type="text" class="form-control" placeholder="Search...">
+                        <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                    </div>
+                    /input-group
+                </li> -->
+                <li>
+                    <a href="/backend/auth/admin/home"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-files-o fa-fw"></i> Pages<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+
+                        <template v-for="(nav,index) in navigation.pages">
+                            <li class="nav-list-li">
+                                <template v-if="nav.sublinks">
+                                    <a :href="nav.link">{{nav.title}}</a>
+                                    <ul class="nav nav-third-level">
+                                        <li class="nav-list-li" v-for="(sublink, index) in nav.sublinks_list">
+                                            <a :href="sublink.link">{{sublink.title}}</a>
+                                        </li>
+                                    </ul>
+                                </template>
+                                <template v-else>
+                                    <a :href="nav.link">{{nav.title}}</a>
+                                </template>
+                            </li>
+
+                        </template>
+                    </ul>
+                    <!-- /.nav-second-level -->
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="blank.html">Blank Page</a>
+                        </li>
+                        <li>
+                            <a href="/backend/pages/login.html">Login Page</a>
+                        </li>
+                    </ul>
+                    <!-- /.nav-second-level -->
+                </li>
+            </ul>
+        </div>
+        <!-- /.sidebar-collapse -->
+    </div>
+    <!-- /.navbar-static-side -->
+</nav>
