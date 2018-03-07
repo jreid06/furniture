ca7686bafe00fe017d883cc4e9462d6f090d260e
jason reid <jasonbrandon9406@gmail.com>
2018-03-07 14:03:03 +0000

    renamed file; added script code to add image successfuly to directory

--- backend/scripts/uploadimages.php
+++ backend/scripts/uploadimages.php
@@ -1,68 +1,65 @@
 <?php
+
     include '../../scripts/dbconnect.php';
-    if (isset($_FILES['attachments'])) {
-        // var_dump($_FILES);
-        // echo $_FILES['attachments']['name'][0];
+    $output_dir = "../../assets/test/";
 
-        // adds file to database
-        $file_name = preg_replace('(\s)','_',$_FILES['attachments']['name'][0]);
+    if(isset($_FILES['img'])){
 
-        $target_dir = "upload/";
-        $target_file = $target_dir.basename($file_name);
-        $size = $_FILES['attachments']['size'][0];
-        $size_len = strlen($size);
-        $sizeRep = str_replace(',', '.', number_format($size));
+    	$fileCount = count($_FILES["img"]["name"]);
+        $res = 'multiple';
 
-        $finalSize = add_size_suffix($size_len, $sizeRep);
+      	$fileName = explode(' ',$_FILES["img"]["name"][0]);
+        $fileName = implode('-', $fileName);
+
+        $file_type_allowed = preg_match("/.\.(jpg|png|jpeg)$/i", $fileName);
+        if (!$file_type_allowed) {
+            $msg = array(
+                'status'=>array(
+                    'code'=>404,
+                    'code_status'=>'error'
+                ),
+                'data'=> array(
+                    'msg'=>'File type uploaded not allowed. Only (jpg | png | jpeg )'
+                )
+            );
+
+        }else {
+            move_uploaded_file($_FILES["img"]["tmp_name"][0],$output_dir.$fileName);
 
 
-        $filetypeallowed = preg_match("/.\.(gif|jpg|png|jpeg)$/i", $file_name);
-
-        if (!$filetypeallowed) {
-            $msg = array('status'=>2,'msg'=>'Only (gif | jpg | png | jpeg )', 'file'=>$file_name);
-
-            exit(json_encode($msg));
+            $msg = array(
+                'status'=>array(
+                    'code'=>101,
+                    'code_status'=>'success'
+                ),
+                'data'=> array(
+                    'msg'=>'successfull added to folder response',
+                    'files'=>$_FILES,
+                    'res'=>$res,
+                    'name'=>$fileName,
+                    'temp_name'=> $_FILES['img']['tmp_name'],
+                    'real_path'=> pathinfo($fileName),
+                    'dir'=>$root_dir.$fileName,
+                    'typeAllowed'=>$file_type_allowed
+                )
+            );
         }
 
-        if (file_exists($target_file)) {
-            $msg = array('status'=>0, 'msg'=>'file already exists', 'filePath'=>$target_file, 'fileName'=>$file_name);
-
-            exit(json_encode($msg));
-        }
-
-        // 2MB
-        if ($_FILES['attachments']['size'][0] >  2000000) {
-            $msg = array('status'=>1, 'msg'=>'file size to large', 'file'=>$target_file, 'fileName'=>$file_name);
-
-            exit(json_encode($msg));
-        }
-
-        if (move_uploaded_file($_FILES['attachments']['tmp_name'][0], $target_file)) {
-            $current_date = getdate();
-            $timestamp = $current_date[0];
-
-            $readable_date = date('m/d/Y H:i:s', $timestamp);
-            $time = date('H:i:s', $timestamp);
-            $date_now = date('m/d/Y', $timestamp);
-            $month = $current_date['month'];
-            $weekday = $current_date['weekday'];
-
-            $loggedInUser = isset($_COOKIE['_unm'])?$_COOKIE['_unm']:$_SESSION['_unm'];
-            // $uploaded_file_data = array(
-            //     'location'=>$target_file,
-            //     ''
-            // )
 
 
-            DatabaseFunctions::addGalleryImages('galleryImages', $target_file, 'Gallery image', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.(Change Me)', $finalSize, $loggedInUser, $readable_date, $time, $month, $weekday);
+        exit(json_encode( $msg ));
+    }else {
+        $msg = array(
+            'status'=>array(
+                'code'=>404,
+                'code_status'=>'error'
+            ),
+            'data'=> array(
+                'msg'=>'test error response. no files sent'
+            )
+        );
 
-            Connect::checkConnection();
-
-            // get the data of the uploaded file SO IT CAN BE USED TO render new table row
-            $uploaded_file_data = DatabaseFunctions::getData('*','galleryImages','imgAddress', $target_file, false);
-
-            $msg = array('status'=>7, 'msg'=>'file uploaded successfully to', 'file'=>$target_file, 'readable_size'=>$finalSize, 'uploaded_file_data'=>$uploaded_file_data);
-
-            exit(json_encode($msg));
-        }
+        exit(json_encode( $msg ));
     }
+
+ ?>
