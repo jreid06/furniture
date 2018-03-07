6fcc241da741721fa58486bfec1eb9b6f118cc37
jason reid <jasonbrandon9406@gmail.com>
2018-03-07 14:06:39 +0000

    removed temp data from dashboard home

--- backend/home.php
+++ backend/home.php
@@ -16,10 +16,27 @@
                     <h1 class="page-header">Dashboard</h1>
                 </div>
                 <!-- /.col-lg-12 -->
+                <div class="col-lg-12">
+                    <h4>Key</h4>
+                    <hr>
+                    <ul class="nav nav-pills" role="tablist">
+                        <li role="presentation" style="background-color: #F5F5F5"><a href="#">GENERAL</a></li>
+                        <li role="presentation" style="background-color: #dff0d8"><a href="#">EDIT</a></li>
+                        <li role="presentation" style="background-color: #D9EDF7"><a href="#">HELP</a></li>
+                    </ul>
+                    <hr>
+                    <br>
+                    <br>
+                </div>
             </div>
             <!-- /.row -->
+
             <div class="row">
-                <div class="col-lg-3 col-md-6">
+
+            </div>
+
+            <div class="row">
+                <!-- <div class="col-lg-6 col-md-6">
                     <div class="panel panel-primary">
                         <div class="panel-heading">
                             <div class="row">
@@ -40,73 +57,63 @@
                             </div>
                         </a>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-green">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-tasks fa-5x"></i>
+                </div> -->
+                <template v-for="(panel, index) in dashboardhome.panels">
+
+                    <template v-if="panel.multiple_links">
+                        <template v-for="(sublinks, index) in panel.link">
+                            <div class="col-lg-4 col-md-4">
+                                <div :class="'panel '+panel.className">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-edit fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-center">
+                                                <div><h4>{{panel.title}}</h4></div>
+                                                <div>{{sublinks.category}}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a :href="sublinks.link">
+                                        <div class="panel-footer">
+                                            <span class="pull-left">View Details</span>
+                                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
                                 </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">12</div>
-                                    <div>New Tasks!</div>
+                            </div>
+                        </template>
+                    </template>
+
+                    <template v-else>
+                        <div class="col-lg-4 col-md-4">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                            <i class="fa fa-edit fa-5x"></i>
+                                        </div>
+                                        <div class="col-xs-9 text-center">
+                                            <div><h4>{{panel.title}}</h4></div>
+                                        </div>
+                                    </div>
                                 </div>
+                                <a :href="panel.link">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">View Details</span>
+                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
                             </div>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-shopping-cart fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">124</div>
-                                    <div>New Orders!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-red">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-support fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>Support Tickets!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+                    </template>
+
+                </template>
+
+
             </div>
             <!-- /.row -->
             <div class="row">
