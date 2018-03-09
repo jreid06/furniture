8eeb81bda4f577e01a031e22872427ccd7c3251d
jason reid <jasonbrandon9406@gmail.com>
2018-03-09 10:47:05 +0000

    script to upload slide images

---
+++ backend/scripts/uploadslideimages.php
@@ -0,0 +1,65 @@
+<?php
+
+    include '../../scripts/dbconnect.php';
+    $output_dir = "../../assets/slideshow/";
+
+    if(isset($_FILES['img'])){
+
+    	$fileCount = count($_FILES["img"]["name"]);
+        $res = 'multiple';
+
+      	$fileName = explode(' ',$_FILES["img"]["name"][0]);
+        $fileName = implode('-', $fileName);
+
+        $file_type_allowed = preg_match("/.\.(jpg|png|jpeg)$/i", $fileName);
+        if (!$file_type_allowed) {
+            $msg = array(
+                'status'=>array(
+                    'code'=>404,
+                    'code_status'=>'error'
+                ),
+                'data'=> array(
+                    'msg'=>'File type uploaded not allowed. Only (jpg | png | jpeg )'
+                )
+            );
+
+        }else {
+            move_uploaded_file($_FILES["img"]["tmp_name"][0],$output_dir.$fileName);
+
+
+            $msg = array(
+                'status'=>array(
+                    'code'=>101,
+                    'code_status'=>'success'
+                ),
+                'data'=> array(
+                    'msg'=>'successfull added to folder response',
+                    'files'=>$_FILES,
+                    'res'=>$res,
+                    'name'=>$fileName,
+                    'temp_name'=> $_FILES['img']['tmp_name'],
+                    'real_path'=> pathinfo($fileName),
+                    'dir'=>$root_dir_slide.$fileName,
+                    'typeAllowed'=>$file_type_allowed
+                )
+            );
+        }
+
+
+
+        exit(json_encode( $msg ));
+    }else {
+        $msg = array(
+            'status'=>array(
+                'code'=>404,
+                'code_status'=>'error'
+            ),
+            'data'=> array(
+                'msg'=>'test error response. no files sent'
+            )
+        );
+
+        exit(json_encode( $msg ));
+    }
+
+ ?>
