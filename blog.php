07ca3eb675043114a40dca97715a926ccc1ca226
jason reid <jasonbrandon9406@gmail.com>
2018-03-15 16:59:00 +0000

    all blog posts page and scripting

---
+++ blog.php
@@ -0,0 +1,56 @@
+<?php
+
+    include dirname(__DIR__).'/idyldev/scripts/dbconnect.php';
+    include ROOT_PATH.'templates/header.php';
+    include ROOT_PATH.'templates/nav.php';
+
+
+    Connect::checkConnection();
+    $blog_posts = DatabaseFunctions::getDataLimit('*', 'blog', 'status', 'published', false, false);
+
+    if ($blog_posts[0]) {
+        $blog_posts = $blog_posts[1];
+        $error = false;
+
+        // adding formatted date information to each post before rendering
+        for ($i=0; $i < count($blog_posts); $i++) {
+            $timestamp = $blog_posts[$i]['date_published'];
+            $formatted_ts = format_timestamp($timestamp);
+
+            $blog_posts[$i]['date'] = array(
+                'time' => $formatted_ts['time'],
+                'day' => $formatted_ts['day'],
+                'month' => $formatted_ts['month'],
+                'year' => $formatted_ts['year'],
+                'full' => $formatted_ts['full_date']
+            );
+        }
+
+    }else {
+        $error = true;
+    }
+
+ ?>
+
+ <div class="container-fluid home all-blog-posts">
+     <div class="row">
+         <div class="col-12">
+             <h3 class="text-center">All Blog Posts</h3>
+             <br>
+         </div>
+     </div>
+     <div class="row">
+         <?php
+            $template = $twig->load('blog-post-home.html.twig');
+            echo $template->render(array(
+                'error'=> $error,
+                'months'=> $months,
+                'posts'=>$blog_posts
+            ));
+         ?>
+     </div>
+ </div>
+
+ <?php
+        include ROOT_PATH.'templates/footer.php';
+  ?>
