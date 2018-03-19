20dded62cb2c9d921519100e279c5d259563090d
jason reid <jasonbrandon9406@gmail.com>
2018-03-19 16:16:52 +0000

    brands script for rendering brands on front end

--- brands.php
+++ brands.php
@@ -9,7 +9,30 @@
         <h3 class="text-center" style="margin-top: 10px;">Brands</h3>
 
         <div class="row">
-            <template v-for="(brandobj, index) in brands">
+
+            <?php
+
+                $brand_data = DatabaseFunctions::getData('*', 'brands', false, false, true);
+
+                // var_dump($brand_data[1]);
+                $brand_parsed_data = array();
+
+                foreach ($brand_data[1][0] as $key => $value) {
+                    if(explode('_',$key)[0] === 'ALPH'){
+
+                        if (json_decode( $value, true )) {
+                            array_push($brand_parsed_data, json_decode( $value, true ));
+                        }
+
+                    }
+                }
+
+                $template = $twig->load('brands-main.html.twig');
+                echo $template->render(array(
+                    'brands'=>$brand_parsed_data
+                ));
+             ?>
+            <!-- <template v-for="(brandobj, index) in brands">
 
                 <div class="col-12 col-sm-6 col-lg-4">
                     <br>
@@ -27,7 +50,7 @@
                     <br>
                 </div>
 
-            </template>
+            </template> -->
         </div>
 
 
