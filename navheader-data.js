b4bd3ffd211056731615ccd82f81303fc5dd252a
jason reid <jasonbrandon9406@gmail.com>
2018-03-24 08:16:06 +0000

    old nav data

---
+++ navheader-data.js
@@ -0,0 +1,29 @@
+{
+    address: '#',
+    title: 'living room',
+    slug: 'livingroom'
+}, {
+    address: '#',
+    title: 'kitchen',
+    slug: 'kitchen'
+}, {
+    address: '#',
+    title: 'bedroom',
+    slug: 'bedroom',
+}, {
+    address: '#',
+    title: 'bath',
+    slug: 'bath'
+}, {
+    address: '/brands',
+    title: 'brands'
+}, {
+    address: '/products/gifts',
+    title: 'gifts'
+}, {
+    address: '/our-story',
+    title: 'our story'
+}, {
+    address: '/blog',
+    title: 'blog'
+}
