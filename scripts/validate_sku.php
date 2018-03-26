2f62a6297c0b2b463fa8c2bb0d2c95fb88922639
jason reid <jasonbrandon9406@gmail.com>
2018-03-26 23:26:34 +0100

    reloacated scripts file

---
+++ scripts/validate_sku.php
@@ -0,0 +1,75 @@
+<?php
+    include 'dbconnect.php';
+
+    if (isset($_POST['sku_id'])) {
+
+        $sku_id = $_POST['sku_id'];
+
+        $sku_result = validate_sku($sku_id);
+
+        if ($sku_result['status']['code'] === 101) {
+
+            $sku_cat = $sku_result['sku_attributes']['category'];
+            $table_name = "images_".$sku_cat;
+
+            //check if table exists
+            $table_exists = DatabaseFunctions::checkTableExists($table_name);
+            if (!$table_exists) {
+
+                $msg = array(
+                    'status'=> array(
+                        'code'=>101,
+                        'code_status'=>'success'
+                    ),
+                    'data'=> array(
+                        'msg'=>'Valid SKU entered. Add images below',
+                        'exists'=>$table_exists
+                    )
+                );
+            }else {
+                // check whether sku data already exists
+                $sku_check = DatabaseFunctions::checkDataExists($table_name, 'sku_id', $sku_id);
+
+                if ($sku_check) {
+
+                    $msg = array(
+                        'status'=> array(
+                            'code'=>404,
+                            'code_status'=>'warn'
+                        ),
+                        'data'=> array(
+                            'msg'=>'Images already added for the SKU ID entered'
+                        )
+                    );
+
+                }else {
+
+                    $msg = array(
+                        'status'=> array(
+                            'code'=>101,
+                            'code_status'=>'success'
+                        ),
+                        'data'=> array(
+                            'msg'=>'Valid SKU entered. Add images below',
+                            'exists'=>$table_exists
+                        )
+                    );
+                }
+
+            }
+
+        }else {
+            $msg = array(
+                'status'=> array(
+                    'code'=>404,
+                    'code_status'=>'error'
+                ),
+                'data'=> array(
+                    'msg'=>'Invalid SKU ID. Please enter a valid SKU'
+                )
+            );
+        }
+
+        exit(json_encode( $msg ));
+    }
+ ?>
