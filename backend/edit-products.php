453caff58a83011f212ca88cb7f380e8c2afccd6
jason reid <jasonbrandon9406@gmail.com>
2018-03-26 09:32:22 +0100

    ammended script to load all products in categories dynamically

--- backend/edit-products.php
+++ backend/edit-products.php
@@ -9,64 +9,23 @@
     $url_product_cat = $url_split[6];
 
     // get data from specific tables or all from IDYL database
-    if ($url_product_cat === 'all') {
-        # get all product data
+
+    $database_name = $database_extension.$url_product_cat;
+    $page_products = DatabaseFunctions::getData('*', $database_name, false, false, true);
+
+    if ($page_products[0]) {
+        $error_status = false;
+    }else {
+        $error_status = true;
     }
 
-    if ($url_product_cat === 'livingroom') {
-        # get all livingroom product data
-        $database_name = $database_extension.$url_product_cat;
-        $page_products = DatabaseFunctions::getData('*', $database_name, false, false, true);
-
-        if ($page_products[0]) {
-            $error_status = false;
-        }else {
-            $error_status = true;
-        }
-    }
-
-    if ($url_product_cat === 'kitchen') {
-        # get all livingroom product data
-        $database_name = $database_extension.$url_product_cat;
-        $page_products = DatabaseFunctions::getData('*', $database_name, false, false, true);
-
-        if ($page_products[0]) {
-            $error_status = false;
-        }else {
-            $error_status = true;
-        }
-    }
-
-    if ($url_product_cat === 'bedroom') {
-        # get all livingroom product data
-        $database_name = $database_extension.$url_product_cat;
-        $page_products = DatabaseFunctions::getData('*', $database_name, false, false, true);
-
-        if ($page_products[0]) {
-            $error_status = false;
-        }else {
-            $error_status = true;
-        }
-    }
-
-    if ($url_product_cat === 'bath') {
-        # get all livingroom product data
-        $database_name = $database_extension.$url_product_cat;
-        $page_products = DatabaseFunctions::getData('*', $database_name, false, false, true);
-
-        if ($page_products[0]) {
-            $error_status = false;
-        }else {
-            $error_status = true;
-        }
-    }
  ?>
 
  <body>
      <div id="wrapper" class="dash-vue">
 
      <?php include "../backend/templates/nav.php" ?>
-     
+
      <?php
          $template = $twig->load('edit-product-images.html.twig');
          echo $template->render(array(
